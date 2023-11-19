704ddda1033e9bfe2c2357c9e9e9914729bfa85b
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-18 23:15:57 -0500

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -22,3 +22,24 @@ class Biblioteca:
         print("Lista de libros en la biblioteca:")
         for libro in self.libros:
             print(f"Título: {libro.titulo}, Autor: {libro.autor}, ISBN: {libro.isbn}")
+# Ejemplo de uso
+if __name__ == "__main__":
+    # Crear algunos libros
+    libro1 = Libro("Python Crash Course", "Eric Matthes", "9781593279288")
+    libro2 = Libro("Clean Code", "Robert C. Martin", "9780132350884")
+    libro3 = Libro("The Pragmatic Programmer", "Andrew Hunt, David Thomas", "9780201616224")
+
+    # Crear una biblioteca
+    biblioteca = Biblioteca()
+
+    # Agregar libros a la biblioteca
+    biblioteca.agregar_libro(libro1)
+    biblioteca.agregar_libro(libro2)
+    biblioteca.agregar_libro(libro3)
+
+    # Mostrar la lista de libros en la biblioteca
+    biblioteca.mostrar_libros()
+
+    # Buscar un libro por título
+    biblioteca.buscar_libro("Clean Code")
+    biblioteca.buscar_libro("Learning Python")
