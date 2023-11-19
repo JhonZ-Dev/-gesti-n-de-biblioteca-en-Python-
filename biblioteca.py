ca1e67faffb778d85471b37ebb3a03891200d8cb
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-18 23:15:08 -0500

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -7,3 +7,7 @@ class Libro:
 class Biblioteca:
     def __init__(self):
         self.libros = []
+    
+     def agregar_libro(self, libro):
+        self.libros.append(libro)
+        print(f"Libro '{libro.titulo}' agregado a la biblioteca.")
