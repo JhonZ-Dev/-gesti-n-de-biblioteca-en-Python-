2ea88f542c57b7daff5e4733ffb03ab30c1421c8
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-18 23:15:28 -0500

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -8,6 +8,13 @@ class Biblioteca:
     def __init__(self):
         self.libros = []
     
-     def agregar_libro(self, libro):
+    def agregar_libro(self, libro):
         self.libros.append(libro)
+        
         print(f"Libro '{libro.titulo}' agregado a la biblioteca.")
+    def buscar_libro(self, titulo):
+        for libro in self.libros:
+            if libro.titulo.lower() == titulo.lower():
+                print(f"Libro encontrado - Título: {libro.titulo}, Autor: {libro.autor}, ISBN: {libro.isbn}")
+                return
+        print(f"Libro con el título '{titulo}' no encontrado en la biblioteca.")
